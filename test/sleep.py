a073f135abfd7a412c0433baf1c6732845ef5b68
Nikolay Zinov <nzinov@gmail.com>
2016-10-20 15:21:21 +0300

    add new test for sleep

---
+++ test/sleep.py
@@ -0,0 +1,20 @@
+"""3
+2
+ :Z
+.+.
+<:T
+2
+ :T
+.+.
+S|E
+2
+ :A 
+.+.
+A: 
+T Stuff({"сокровище": 1})
+E Exit(RIGHT)
+< Exit(LEFT)
+A Armory()
+Z EffectorSquare(lambda: Sleep(5, Position(1, 0, 0)))
+S EffectorSquare(lambda: Sleep(3, Position(2, 0, 0)))
+"""
