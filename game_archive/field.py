a52545d6fc785d23a8f86a5b3bc42988c2cff2b7
Nikolay Zinov <nzinov@gmail.com>
2019-01-30 14:20:51 +0300

    Update old game for current format

--- game_archive/field.py
+++ game_archive/field.py
@@ -1,3 +1,51 @@
+"""
+3
+8
+ : | :V: |W:R:M
+.+.+.+.+.+.+.+.
+ :^: : : : :R:R
+.+.+.+.+.+=+.+=
+ | :B| : |G: : 
+.+.+=+.+=+.+.+.
+ : : : : : | : 
+.+.+.+.+=+.+.+=
+W: : : :<: :V: 
+=+.+=+.+=+.+.+.
+G: : : |T| | :W
+=+=+.+=+.+.+.+=
+>:Z: : | : |A: 
+.+.+.+.+=+=+.+.
+@: :E: : : : : 
+4
+ :G: : 
+.+.+.+.
+ :O:D: 
+.+=+.+.
+>: :<: 
+.+.+.+.
+ |^| : 
+3
+T:M:T
+.+.+.
+T:O:T
+.+.+.
+T:E:T
+T Stuff({"сокровище": 1})
+E Exit(DOWN)
+M Minotaur()
+R Square("Вы слышите РЫК МИНОТАВРА!")
+W Stuff({"посох": 1})
+A Hole((2, 2))
+B Hole((6, 6))
+G Armory()
+^ RubberRoom(UP)
+> RubberRoom(RIGHT)
+< RubberRoom(LEFT)
+V RubberRoom(DOWN)
+Z EffectorSquare(lambda: Sleep(7, 1, (1, 1)))
+D EffectorSquare(lambda: Sleep(4, 2, (1, 1)))
+@ EffectorSquare(lambda: Stun(3), "Вы попали в ловушку!")
+"""
 from squares import Square
 from objects import register_object, Object
 class Minotaur(Square):
@@ -18,5 +66,3 @@ class Bullet(Object):
             else:
                 game.log('"Петух! Петух!" - раздается голос из ниоткуда, и опять звучит странный смех.')
             return False
-
-
